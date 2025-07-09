dfb1fbbcbb43e9bf383b78cb96d0596500be0d9f
David Rios <david@davidrios.co>
2025-07-08 22:49:20 -0500

    📦 Package: Added SVG support

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgrg from 'vite-plugin-svgr';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths(), svgrg()],
 });
